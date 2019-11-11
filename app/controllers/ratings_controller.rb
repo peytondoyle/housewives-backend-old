fe095af5a190e17fcbd7401e59d0ffab0a63d662
Peyton Doyle <p6doyle@gmail.com>
2019-11-11 09:04:13 -0500

    require params

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -44,7 +44,8 @@ class RatingsController < ActionController::API
     private
 
     def rating_params
-      params.permit(:rating, :user_id, :housewife_id)
+      Require(:rating).permit(:rating, :user_id, :housewife_id)
+      # params.permit(:rating, :user_id, :housewife_id)
       # params.permit(:rating, :user_id, :housewife_id, :id)
     end
 
