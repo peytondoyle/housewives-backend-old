682d783571c98f68db06e511881595c6e8811f97
Peyton Doyle <p6doyle@gmail.com>
2019-11-12 12:30:20 -0500

    fixed relationship issue

--- app/models/housewife.rb
+++ app/models/housewife.rb
@@ -1,6 +1,7 @@
 class Housewife < ApplicationRecord
   has_many :taglines
   has_many :ratings
+  has_many :favorites
   has_many :seasons, through: :taglines
   has_many :users, through: :ratings
 
