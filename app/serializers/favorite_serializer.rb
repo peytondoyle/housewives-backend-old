af5036aa5877d2a74ca5450352e7b54ab4f263e4
Peyton Doyle <p6doyle@gmail.com>
2019-11-13 07:38:59 -0500

    serializing again?

--- app/serializers/favorite_serializer.rb
+++ app/serializers/favorite_serializer.rb
@@ -1,5 +1,13 @@
 class FavoriteSerializer < ActiveModel::Serializer
-  attributes :id, :housewife_id, :user_id
+  attributes :id, :housewife_id, :user_id, :housewife
   belongs_to :housewife
   belongs_to :user
+
+  def housewife
+    self.object.housewives.map do |fav_obj|
+      fav_obj.id
+    end
+  end
+
 end
+
